52a81bf4361d304a73aa1d81ff3b7360aebb9745
CoderGamester <game.gamester@gmail.com>
2023-07-28 16:32:38 +0100

    Code improved for bugs and flow fixes Add OpenAI code reviewer action Update package number

--- Runtime/CsvParser.cs
+++ Runtime/CsvParser.cs
@@ -16,16 +16,72 @@ namespace GameLovers.GoogleSheetImporter
 	/// </summary>
 	public static class CsvParser
 	{
-		private const char _ignoreColumnChar = '$';
-		private static readonly char[] _pairSplitChars = new[] {':', '<', '>', '='};
-		private static readonly char[] _arraySplitChars = new[] {',', '(', ')', '[', ']', '{', '}'};
+		public const string IGNORE_COLUMN_CHAR = "$";
+		public const string IGNORE_FIELD_CHAR = "#";
+		public const string SUB_LIST_SUFFIX = "[]";
+
+		public static readonly char[] PairSplitChars = { ':', '<', '>', '=', '|' };
+		public static readonly char[] ArraySplitChars = { ',', '(', ')', '[', ']', '{', '}' };
+		public static readonly string[] NewLineChars = { "\r\n", "\r", "\n" };
 
 		/// <summary>
-		/// Deserializes the given CSV <paramref name="data"/> cell values to an object of the given <typeparamref name="T"/> type
+		/// Parses the entire <paramref name="csv"/> text.
+		/// Each row is an element on the returned list
+		/// Each column is an element on the returned dictionary. The dictionary key will be the CSV header
 		/// </summary>
-		public static T DeserializeTo<T>(Dictionary<string, string> data)
+		public static List<Dictionary<string, string>> ConvertCsv(string csv)
 		{
-			var type = typeof(T);
+			var lines = csv.Split(new[] { "\r\n" }, StringSplitOptions.None);
+			var list = new List<Dictionary<string, string>>(lines.Length - 1);
+			var headlines = EnumerateCsvLine(lines[0]);
+
+			for (var i = 1; i < lines.Length; i++)
+			{
+				var dictionary = new Dictionary<string, string>(headlines.Length);
+				var values = EnumerateCsvLine(lines[i]);
+
+				for (var j = 0; j < headlines.Length; j++)
+				{
+					if (headlines[j].StartsWith(IGNORE_COLUMN_CHAR))
+					{
+						continue;
+					}
+
+					// fix for any extra invalid columns
+					// two empty columns in a row are parsed as a single empty value 
+					// e.g (one,two,,) ir parsed as [one, two, ""]
+					if (j >= values.Length)
+					{
+						dictionary.Add(headlines[j], "");
+					}
+					else
+					{
+						dictionary.Add(headlines[j], values[j].Trim());
+					}
+				}
+
+				list.Add(dictionary);
+			}
+
+			return list;
+		}
+
+		/// <inheritdoc cref="DeserializeTo"/>
+		public static T DeserializeTo<T>(Dictionary<string, string> data,
+										 params Func<string, Type, object>[] deserializers)
+		{
+			return (T)DeserializeTo(typeof(T), data, deserializers);
+		}
+
+		/// <summary>
+		/// Deserializes the given CSV <paramref name="data"/> cell values to an object of the given <paramref name="type"/> type
+		/// </summary>
+		/// <remarks>
+		/// It provides extra custom <paramref name="deserializers"/> to specific parsing
+		/// </remarks>
+		public static object DeserializeTo(Type type, Dictionary<string, string> data,
+										   params Func<string, Type, object>[] deserializers)
+		{
 			var ignoreType = typeof(ParseIgnoreAttribute);
 			var instance = Activator.CreateInstance(type);
 
@@ -40,74 +96,114 @@ namespace GameLovers.GoogleSheetImporter
 				{
 					continue;
 				}
-				
-				var stringSerialized = GetStringSerialized(field, data[field.Name]);
-				
-				field.SetValue(instance, JsonConvert.DeserializeObject(stringSerialized, field.FieldType));
+
+				field.SetValue(instance, DeserializeObject(data[field.Name], field.FieldType, deserializers));
 			}
 
-			return (T) instance;
+			return instance;
 		}
 
 		/// <summary>
-		/// Requests the given <paramref name="data"/> value in a serializable format to fill the given <paramref name="field"/>
+		/// Deserializes the given <paramref name="data"/> to the given <paramref name="type"/> in a <see cref="object"/>
+		/// format as a result to be set via reflection.
 		/// </summary>
-		public static string GetStringSerialized(FieldInfo field, string data)
+		/// <remarks>
+		/// It provides extra custom <paramref name="deserializers"/> to specific parsing
+		/// </remarks>
+		public static object DeserializeObject(string data, Type type,
+											   params Func<string, Type, object>[] deserializers)
 		{
 			var listType = typeof(IList);
 			var dictionaryType = typeof(IDictionary);
-			
-			if (field.FieldType.IsArray)
+			var unityDictionaryType = typeof(UnitySerializedDictionary<,>).GetGenericTypeDefinition();
+
+			if (type.IsArray)
 			{
-				return JsonConvert.SerializeObject(ArrayParse(data, null, field.FieldType.GetElementType()));
+				return ArrayParse(data, type.GetElementType(), deserializers);
 			}
-			if (listType.IsAssignableFrom(field.FieldType))
+
+			if (listType.IsAssignableFrom(type))
 			{
-				return JsonConvert.SerializeObject(ArrayParse(data, null, field.FieldType.GenericTypeArguments[0]));
+				return ArrayParse(data, type.GenericTypeArguments[0], deserializers);
 			}
-			if (dictionaryType.IsAssignableFrom(field.FieldType))
+
+			if (type.BaseType != null && type.BaseType.IsGenericType && type.BaseType.GetGenericTypeDefinition()
+					.IsAssignableFrom(unityDictionaryType))
 			{
-				var types = field.FieldType.GenericTypeArguments;
-					
-				return JsonConvert.SerializeObject(DictionaryParse(data, null, types[0], types[1]));
+				var types = type.BaseType.GenericTypeArguments;
+				return DictionaryParse(data, types[0], types[1], type, deserializers);
 			}
-			if (IsKeyValuePairType(field.FieldType))
+
+			if (dictionaryType.IsAssignableFrom(type))
 			{
-				return SerializedKeyValuePair(data);
+				var types = type.GenericTypeArguments;
+				var keyType = types[0];
+				var valueType = types[1];
+				return DictionaryParse(data, keyType, valueType,
+					typeof(Dictionary<,>).MakeGenericType(keyType, valueType), deserializers);
 			}
-			return $"\"{data}\"";
+
+			return Parse(data, type, deserializers);
 		}
-		
+
 		/// <summary>
-		/// Parses the entire <paramref name="csv"/> text.
-		/// Each row is an element on the returned list
-		/// Each column is an element on the returned dictionary. The dictionary key will be the CSV header
+		/// Deserializes a list of custom complex (non-primitive) types, with their own headers / definitions.
 		/// </summary>
-		public static List<Dictionary<string, string>> ConvertCsv(string csv)
+		public static object DeserializeSubList(List<Dictionary<string, string>> data, int startIndex, Type type,
+												string fieldName,
+												params Func<string, Type, object>[] deserializers)
 		{
-			var lines = csv.Split(new [] { "\r\n" }, StringSplitOptions.None);
-			var list = new List<Dictionary<string, string>>(lines.Length - 1);
-			var headlines = EnumerateCsvLine(lines[0]);
+			var subType = type.GetGenericArguments()[0];
+			var subData = GetSubListDictionary(data, startIndex);
 
-			for (var i = 1; i < lines.Length; i++)
+			var listType = typeof(List<>).MakeGenericType(subType);
+			var list = Activator.CreateInstance(listType);
+			var addMethod = listType.GetMethod("Add")!;
+
+			foreach (var dict in subData)
 			{
-				var dictionary = new Dictionary<string, string>(headlines.Length);
-				var values = EnumerateCsvLine(lines[i]);
-
-				for (var j = 0; j < headlines.Length; j++)
-				{
-					if (headlines[j][0] == _ignoreColumnChar)
-					{
-						continue;
-					}
-					
-					dictionary.Add(headlines[j], values[j].Trim());
-				}
-				
-				list.Add(dictionary);
+				addMethod.Invoke(list, new[] { DeserializeTo(subType, dict, deserializers) });
 			}
 
 			return list;
+		}
+
+		/// <summary>
+		/// Extracts the data dictionary of a sub list from the base deserialization data of an object.
+		/// </summary>
+		private static List<Dictionary<string, string>> GetSubListDictionary(
+			List<Dictionary<string, string>> data, int startIndex)
+		{
+			var headerMap = new Dictionary<string, string>();
+
+			foreach (var (key, value) in data[startIndex])
+			{
+				if (value.EndsWith(SUB_LIST_SUFFIX)) continue;
+
+				headerMap.Add(key, value);
+			}
+
+			var objData = new List<Dictionary<string, string>>();
+			for (int i = startIndex + 1; i < data.Count; i++)
+			{
+				var dataLine = data[i];
+
+				if (dataLine["Key"] != IGNORE_FIELD_CHAR) break;
+
+				var rowData = new Dictionary<string, string>();
+
+				foreach (var (key, value) in dataLine)
+				{
+					if (headerMap.TryGetValue(key, out var header))
+					{
+						rowData.Add(header, value);
+					}
+				}
+
+				objData.Add(rowData);
+			}
+
+			return objData;
 		}
 
 		/// <summary>
@@ -115,29 +211,26 @@ namespace GameLovers.GoogleSheetImporter
 		/// A text is in array format as long as is divided by ',', '{}', '()' or '[]' (ex: 1,2,3; {1,2}{4,5}, [1,2,3])
 		/// If the given <paramref name="text"/> is not in an array format, it will return an array with <paramref name="text"/> as the only element
 		/// </summary>
+		/// <remarks>
+		/// It provides extra custom <paramref name="deserializers"/> to specific parsing
+		/// </remarks>
 		/// <exception cref="FormatException">
 		/// Thrown if the given <paramref name="text"/> is not in the given <typeparamref name="T"/> type format
 		/// </exception>
-		public static List<T> ArrayParse<T>(string text)
+		public static List<T> ArrayParse<T>(string text, params Func<string, Type, object>[] deserializers)
 		{
-			return ArrayParse(text, new List<T>(), typeof(T)) as List<T>;
+			return ArrayParse(text, typeof(T), deserializers) as List<T>;
 		}
 
 		/// <inheritdoc cref="ArrayParse{T}" />
-		public static object ArrayParse(string text, IList list, Type type)
+		public static object ArrayParse(string data, Type type, params Func<string, Type, object>[] deserializers)
 		{
-			var split = text.Split(_arraySplitChars);
-
-			list = list ?? new List<dynamic>();
+			var split = data.Split(ArraySplitChars, StringSplitOptions.RemoveEmptyEntries);
+			var list = (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(type));
 
 			foreach (var value in split)
 			{
-				if (string.IsNullOrEmpty(value))
-				{
-					continue;
-				}
-				
-				list.Add(Parse(value, type));
+				list.Add(Parse(value, type, deserializers));
 			}
 
 			return list;
@@ -150,47 +243,60 @@ namespace GameLovers.GoogleSheetImporter
 		/// If the given <paramref name="text"/> is not in an <seealso cref="Dictionary{TKey, TValue}"/> type format,
 		/// it will return an empty dictionary
 		/// </summary>
+		/// <remarks>
+		/// It provides extra custom <paramref name="deserializers"/> to specific parsing
+		/// </remarks>
 		/// <exception cref="FormatException">
 		/// Thrown if the given <paramref name="text"/> is not in the given <typeparamref name="TKey"/> or <typeparamref name="TValue"/> type format
 		/// </exception>
 		/// <exception cref="IndexOutOfRangeException">
 		/// Thrown if the given <paramref name="text"/> has a odd amount of values to pair. Must always be an even amount of values
 		/// </exception>
-		public static Dictionary<TKey, TValue> DictionaryParse<TKey, TValue>(string text)
+		public static Dictionary<TKey, TValue> DictionaryParse<TKey, TValue>(
+			string text, params Func<string, Type, object>[] deserializers)
 		{
-			return DictionaryParse(text, new Dictionary<TKey, TValue>(), typeof(TKey), typeof(TValue)) as Dictionary<TKey, TValue>;
+			var keyType = typeof(TKey);
+			var valueType = typeof(TValue);
+			return DictionaryParse(text, keyType, valueType, typeof(Dictionary<,>).MakeGenericType(keyType, valueType),
+				deserializers) as Dictionary<TKey, TValue>;
 		}
 
 		/// <inheritdoc cref="DictionaryParse{TKey,TValue}" />
-		private static object DictionaryParse(string text, IDictionary dictionary, Type keyType, Type valueType)
+		private static object DictionaryParse(string text, Type keyType, Type valueType, Type dictionaryType,
+											  params Func<string, Type, object>[] deserializers)
 		{
-			var items = ArrayParse<string>(text);
-			
-			dictionary = dictionary ?? new Dictionary<dynamic, dynamic>();
+			var items = ArrayParse<string>(text, deserializers);
+			var dictionary = (IDictionary)Activator.CreateInstance(dictionaryType);
 
-			if (items[0].IndexOfAny(_pairSplitChars) != -1)
+			if (items.Count == 0)
+			{
+				return null;
+			}
+
+			if (items[0].IndexOfAny(PairSplitChars) != -1)
 			{
 				foreach (var item in items)
 				{
-					var split = item.Split(_pairSplitChars);
-					var key = Parse(split[0], keyType);
-					var value = Parse(split[1], valueType);
-					
+					var split = item.Split(PairSplitChars);
+					var key = Parse(split[0], keyType, deserializers);
+					var value = Parse(split[1], valueType, deserializers);
+
 					dictionary.Add(key, value);
 				}
 			}
 			else if (items.Count % 2 == 1)
 			{
-				throw new IndexOutOfRangeException($"Dictionary must have an even amount of values and the following text" +
-				                                   $"has {items.Count.ToString()} values. \nText:{text}");
+				throw new
+					IndexOutOfRangeException($"Dictionary must have an even amount of values and the following text" +
+						$"has {items.Count.ToString()} values. \nText:{text}");
 			}
 			else
 			{
-				for(var i = 0; i < items.Count; i += 2)
+				for (var i = 0; i < items.Count; i += 2)
 				{
-					var key = Parse(items[i], keyType);
-					var value = Parse(items[i + 1], valueType);
-					
+					var key = Parse(items[i], keyType, deserializers);
+					var value = Parse(items[i + 1], valueType, deserializers);
+
 					dictionary.Add(key, value);
 				}
 			}
@@ -201,58 +307,91 @@ namespace GameLovers.GoogleSheetImporter
 		/// <summary>
 		/// Parses the given <paramref name="text"/> to the given <typeparamref name="T"/> type
 		/// </summary>
+		/// <remarks>
+		/// It provides extra custom <paramref name="deserializers"/> to specific parsing
+		/// </remarks>
 		/// <exception cref="FormatException">
 		/// Thrown if the given <paramref name="text"/> is not in the given <typeparamref name="T"/> type format
 		/// </exception>
-		public static T Parse<T>(string text)
+		public static T Parse<T>(string text, params Func<string, Type, object>[] deserializers)
 		{
-			return (T) Parse(text, typeof(T));
+			return (T)Parse(text, typeof(T), deserializers);
 		}
 
 		/// <inheritdoc cref="Parse{T}" />
-		public static object Parse(string text, Type type)
+		public static object Parse(string text, Type type, params Func<string, Type, object>[] deserializers)
 		{
 			text = text.Trim();
-			
+
 			if (type == typeof(string))
 			{
 				return text;
 			}
-			
+
 			if (type.IsEnum)
 			{
 				return Enum.Parse(type, text);
 			}
 
-			if (IsKeyValuePairType(type))
+			if (TryGetKeyValuePair(text, type, out var key, out var value, deserializers))
 			{
-				return JsonConvert.DeserializeObject(SerializedKeyValuePair(text), type);
+				return Activator.CreateInstance(type, key, value);
 			}
-			
+
+			deserializers ??= Array.Empty<Func<string, Type, object>>();
+
+			foreach (var func in deserializers)
+			{
+				var res = func(text, type);
+
+				if (res != null)
+				{
+					return res;
+				}
+			}
+
+			if (type == typeof(DateTime))
+			{
+				return DateTime.Parse(text);
+			}
+
+			if (type == typeof(TimeSpan))
+			{
+				return TimeSpan.Parse(text);
+			}
+
 			//Handling Nullable types i.e, int?, double?, bool? .. etc
-			if (type.IsValueType && Nullable.GetUnderlyingType(type) != null) 
+			if (type.IsValueType && Nullable.GetUnderlyingType(type) != null)
 			{
 				// ReSharper disable once PossibleNullReferenceException
 				return TypeDescriptor.GetConverter(type).ConvertFrom(text);
 			}
 
-			if (type.IsValueType)
+			try
 			{
-				return Convert.ChangeType(text, type);
+				if (type.IsValueType)
+				{
+					return Convert.ChangeType(text, type);
+				}
 			}
-			
-			throw new FormatException($"The text {text} is not convertible to type {type}");
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				throw;
+			}
+
+			return JsonConvert.DeserializeObject($"\"{text}\"", type);
 		}
 
-		private static string[] EnumerateCsvLine(string line) 
+		private static string[] EnumerateCsvLine(string line)
 		{
 			// Regex taken from http://wiki.unity3d.com/index.php?title=CSVReader
 			const string match = @"(((?<x>(?=[,\r\n]+))|""(?<x>([^""]|"""")+)""|(?<x>[^,\r\n]+)),?)";
-			
+
 			var matches = Regex.Matches(line, match, RegexOptions.ExplicitCapture);
 			var ret = new string[matches.Count];
-			
-			for(var i = 0; i < matches.Count; i++) 
+
+			for (var i = 0; i < matches.Count; i++)
 			{
 				ret[i] = matches[i].Groups[1].Value;
 			}
@@ -262,26 +401,38 @@ namespace GameLovers.GoogleSheetImporter
 
 		private static string SerializedKeyValuePair(string text)
 		{
-			var split = text.Split(_pairSplitChars);
+			var split = text.Split(PairSplitChars);
 
 			return $"{{\"Key\":\"{split[0].Trim()}\",\"Value\":\"{split[1].Trim()}\"}}";
 		}
 
-		private static bool IsKeyValuePairType(Type type)
+		private static bool TryGetKeyValuePair(string data, Type type, out object key, out object value,
+											   params Func<string, Type, object>[] deserializers)
 		{
 			if (!type.IsValueType)
 			{
+				key = null;
+				value = null;
+
 				return false;
 			}
 
-			if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(KeyValuePair<,>))
+			var fields = type.GetFields();
+			var split = data.Split(PairSplitChars);
+
+			if (type.IsGenericType && fields.Length == 2 && (fields[0].Name == "Key" || fields[0].Name == "Value1") &&
+				(fields[1].Name == "Value" || fields[1].Name == "Value2"))
 			{
+				key = string.IsNullOrWhiteSpace(data) ? default : Parse(split[0], fields[0].FieldType, deserializers);
+				value = string.IsNullOrWhiteSpace(data) ? default : Parse(split[1], fields[1].FieldType, deserializers);
+
 				return true;
 			}
 
-			var fields = type.GetFields();
+			key = null;
+			value = null;
 
-			return fields.Length == 2 && fields[0].Name == "Key" && fields[1].Name == "Value";
+			return false;
 		}
 	}
 }
